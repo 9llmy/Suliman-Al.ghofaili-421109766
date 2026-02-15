b9857f7518bbd51bc6b5a251f2a137723e38241a
9llmyy <saloom95336@gmail.com>
2026-02-15 15:32:48 +0300

    Finished Lab 3

--- libraryproject/settings.py
+++ libraryproject/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for libraryproject project.
 
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'libraryproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "apps/templates")],     
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
