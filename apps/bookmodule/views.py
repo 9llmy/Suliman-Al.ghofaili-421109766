6b8bfc964ca4f2f6953a90ae13accfb4540f6d19
9llmyy <saloom95336@gmail.com>
2026-04-05 15:02:22 +0300

    Complete Lab 6: Implement search with actual Arabic Islamic books (Al-Bukhari, Al-Dhahabi, Ibn Qayyim)

--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -19,3 +19,27 @@ def listing(request):
     return render(request, 'bookmodule/listing.html')
 def tables(request):
     return render(request, 'bookmodule/tables.html')
+def getBooksList():
+    book1 = {'id': 12344321, 'title': 'كتاب الداء والدواء', 'author': 'ابن قيم الجوزية'}
+    book2 = {'id': 56788765, 'title': 'كتاب سير أعلام النبلاء', 'author': 'الذهبي'}
+    book3 = {'id': 43211234, 'title': 'كتاب صحيح البخاري', 'author': 'الإمام البخاري'}
+    return [book1, book2, book3]
+def search(request):
+    if request.method == "POST":
+        string = request.POST.get('keyword').lower()
+        isTitle = request.POST.get('option1')
+        isAuthor = request.POST.get('option2')
+        
+        # جلب الكتب والفلترة
+        books = getBooksList()
+        newBooks = []
+        for item in books:
+            contained = False
+            if isTitle and string in item['title'].lower(): contained = True
+            if not contained and isAuthor and string in item['author'].lower(): contained = True
+            
+            if contained: newBooks.append(item)
+            
+        return render(request, 'bookmodule/bookList.html', {'books': newBooks})
+    
+    return render(request, 'bookmodule/search.html')
